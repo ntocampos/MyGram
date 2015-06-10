f80b2560f759e1c96a22a8def7f1795d9e151df1
Moisés Neto <ntocampos@gmail.com>
2015-06-10 00:46:34 -0300

    After the functional sidebar.

--- index.php
+++ index.php
@@ -17,14 +17,11 @@ else
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<script type="text/javascript" src="js/angular.min.1.4.js"></script>
-	<script type="text/javascript" src="js/app.js"></script>
-	<script type="text/javascript" src="js/jquery.js"></script>
-	
-	<script type="text/javascript" src="theme/bootstrap.min.js"></script>
-
 	<link rel="stylesheet" type="text/css" href="theme/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="theme/bootswatch.min.css">
+	<link rel="stylesheet" type="text/css" href="css/simple-sidebar.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+
 	<link rel="shortcut icon" type="image/ico" href="assets/favicon.ico"/>
 </head>
 
@@ -66,7 +63,101 @@ else
 		</div>
 	</div>
 
-	<div class="container">
+	<div id="wrapper">
+		<a href="#menu-toggle" class="btn btn-default" id="menu-toggle">
+			<i id="toggle-symbol" class="fa fa-chevron-left fa-lg pull-left"></i>
+		</a>
+		<!-- Sidebar -->
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="#">
+                        Start Bootstrap
+                    </a>
+                </li>
+                <li>
+                    <a href="#">Dashboard</a>
+                </li>
+                <li>
+                    <a href="#">Shortcuts</a>
+                </li>
+                <li>
+                    <a href="#">Overview</a>
+                </li>
+                <li>
+                    <a href="#">Events</a>
+                </li>
+                <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">Services</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /#sidebar-wrapper -->
+
+        <div id="page-content-wrapper">
+        	<div class="container">
+        		<div class="page-header" id="banner">
+					<div class="row">
+						<div class="col-lg-8 col-md-7 col-sm-6">
+							<h1>mygram</h1>
+							<p class="lead">A plus+ to your <u>Instagram</u>&trade;</p>
+						</div>
+						<div class="col-lg-4 col-md-5 col-sm-6">
+							<div class="sponsor">
+
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="page-header">
+							<h1 id="navbar">recent likes</h1>
+						</div>
+					</div>
+				</div>
+				<div class="row" ng-controller="LikeController as userLikes">
+					<div class="col-lg-4" ng-repeat="like in userLikes.likes" align="center">
+						<div class="panel panel-default" style="max-width: 360px">
+							<div class="panel-body">
+								<a href="{{like.link}}">
+								<img ng-src="{{like.images.low_resolution.url}}" alt="{{like.caption.from.username}}">
+								</a>
+							</div>
+							<div class="panel-footer" style="max-height:85px; min-height: 85px">
+								<div id="description-text" ng-show="like.caption.text">{{like.caption.text | limitTo: 85}}{{like.caption.text.length > 85 ? '...' : ''}}, </div>
+								<div>by <a href="http://instagram.com/{{like.user.username}}"><b>{{like.user.username}}</b></a></div>
+							</div>
+						</div>
+					</div>
+				</div>
+        	</div>
+        </div>
+	</div>
+
+
+	<script type="text/javascript" src="js/angular.min.1.4.js"></script>
+	<script type="text/javascript" src="js/app.js"></script>
+	<script type="text/javascript" src="js/jquery.js"></script>
+	
+	<script type="text/javascript" src="theme/bootstrap.min.js"></script>
+
+	<script>
+	$("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+        document.getElementById('toggle-symbol').className = document.getElementById('toggle-symbol').getAttribute('class') == "fa fa-chevron-right fa-lg pull-left" ? "fa fa-chevron-left fa-lg pull-left" : "fa fa-chevron-right fa-lg pull-left";
+    });
+    </script>
+
+<!-- 	<div class="container">
 		<div class="page-header" id="banner">
 			<div class="row">
 				<div class="col-lg-8 col-md-7 col-sm-6">
@@ -103,6 +194,6 @@ else
 				</div>
 			</div>
 		</div>
-	</div>
+	</div> -->
 </body>
 </html>
