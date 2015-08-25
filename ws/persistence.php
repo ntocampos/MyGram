0ec61b8667fb590ade76621ab9ca5032f5caa029
Moisés Neto <ntocampos@gmail.com>
2015-08-25 03:05:00 -0300

    Modificação no config, API Callback.

--- ws/persistence.php
+++ ws/persistence.php
@@ -59,4 +59,35 @@
     mysqli_query($connection, $query_relation);
     mysqli_query($connection, $query_user);
   }
+
+  function addFollows($user_id) {
+    global $instagram, $connection;
+
+    $next_cursor = null;
+    $values_relation = '';
+    $values_user = '';
+    do {
+      $follows = $instagram->getUserFollows($user_id, 92, $next_cursor);
+      $next_cursor = isset($follows->pagination->next_cursor) ? $follows->pagination->next_cursor : null;
+
+      foreach ($follows->data as $follower) {
+        $values_relation .= '("'.$follower->id.'", "'.$user_id.'"),';
+        $values_user .= '("'.$follower->id.'", "'.$follower->username.'", "'.$follower->profile_picture.'", "'.$follower->full_name.'"),';
+      }
+    } while ($next_cursor);
+
+    $values_relation = rtrim($values_relation, ',');
+
+    // ?Antes do delete, salvar em outro canto para comparação futura
+    $delete = "DELETE FROM relation WHERE user_id = ".$user_id;
+    mysqli_query($connection, $delete);
+
+    $query_relation = 'INSERT INTO relation (user_id, followed_by) VALUES' . $values_relation;
+    $query_user =   'INSERT INTO user (id, username, profile_picture, full_name) VALUES '.trim($values_user, ',').' ' .
+                    'ON DUPLICATE KEY UPDATE username = VALUES(username), profile_picture = VALUES(profile_picture), full_name = VALUES(full_name)';
+    echo $query_user;
+
+    mysqli_query($connection, $query_relation);
+    mysqli_query($connection, $query_user);
+  }
 ?>
